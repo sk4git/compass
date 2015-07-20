5361912abfccc0607da93a0e1a0d87f78981e8ed
sk4git <sk84mnp@gmail.com>
2015-07-21 00:02:55 +0530

    Update ProjectRestControllerTest.java

--- src/test/java/compass/ProjectRestControllerTest.java
+++ src/test/java/compass/ProjectRestControllerTest.java
@@ -1,5 +1,8 @@
 package compass;
 
+import static com.jayway.restassured.RestAssured.when;
+import static org.junit.Assert.assertEquals;
+
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
@@ -7,7 +10,6 @@ import java.util.List;
 import org.apache.http.HttpStatus;
 import org.hamcrest.Matchers;
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -17,19 +19,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import com.compass.microservices.Application;
 import com.compass.microservices.model.Project;
 import com.compass.microservices.repository.ProjectRepository;
-import com.compass.microservices.services.ProjectService;
 import com.jayway.restassured.RestAssured;
-import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
-import com.mongodb.DBCollection;
 import com.mongodb.Mongo;
 
 import de.flapdoodle.embed.mongo.MongodExecutable;
@@ -39,69 +36,62 @@ import de.flapdoodle.embed.mongo.config.MongodConfig;
 import de.flapdoodle.embed.mongo.config.RuntimeConfig;
 import de.flapdoodle.embed.mongo.distribution.Version;
 import de.flapdoodle.embed.process.extract.UserTempNaming;
-import static com.jayway.restassured.RestAssured.*;
-import static org.junit.Assert.assertEquals;
 
-
-@RunWith(SpringJUnit4ClassRunner.class)   
-@SpringApplicationConfiguration(classes = Application.class)   
-@WebAppConfiguration   
-@IntegrationTest("server.port:0")  
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+@IntegrationTest("server.port:0")
 public class ProjectRestControllerTest {
-	
+
 	private static final String LOCALHOST = "127.0.0.1";
-    private static final String DB_NAME = "itest";
-    private static final int MONGO_TEST_PORT = 27017;
-    
-    private static MongodProcess mongoProcess;
-    
-    private static Mongo mongo;
-    private MongoTemplate template;
+	private static final String DB_NAME = "itest";
+	private static final int MONGO_TEST_PORT = 27017;
 
+	private static MongodProcess mongoProcess;
 
-    
-	
+	private static Mongo mongo;
+	private static MongoTemplate template;
+
 	@Autowired
 	ProjectRepository projectRepository;
-	
-    Project fNf;
-    Project horizon;
+
+	Project fNf;
+	Project horizon;
 	Project compass;
 
+	@Value("${local.server.port}")
+	// 6
+	int port;
 
-    @Value("${local.server.port}")   // 6
-    int port;
-    
-    @BeforeClass
-    public static void initializeDB() throws IOException {
- 
-        RuntimeConfig config = new RuntimeConfig();
-        config.setExecutableNaming(new UserTempNaming());
- 
-        MongodStarter starter = MongodStarter.getInstance(config);
- 
-        MongodExecutable mongoExecutable = starter.prepare(new MongodConfig(Version.V2_2_0, MONGO_TEST_PORT, false));
-        mongoProcess = mongoExecutable.start();
- 
-        mongo = new Mongo(LOCALHOST, MONGO_TEST_PORT);
-        DB db = mongo.getDB(DB_NAME);
-        //DBCollection col = db.createCollection("projects", new BasicDBObject());
+	@BeforeClass
+	public static void initializeDB() throws IOException {
 
-      }
-    
-    @AfterClass
-    public static void shutdownDB() throws InterruptedException {
-        mongo.close();
-        mongoProcess.stop();
-    }
-    
-    @After
-    public void tearDown() throws Exception {
-       template.dropCollection(Project.class);
-    }
-   	
-    @Before
-    public void setUp() {
+		RuntimeConfig config = new RuntimeConfig();
+		config.setExecutableNaming(new UserTempNaming());
+
+		MongodStarter starter = MongodStarter.getInstance(config);
+
+		MongodExecutable mongoExecutable = starter.prepare(new MongodConfig(
+				Version.V2_2_0, MONGO_TEST_PORT, false));
+		mongoProcess = mongoExecutable.start();
+
+		mongo = new Mongo(LOCALHOST, MONGO_TEST_PORT);
+		DB db = mongo.getDB(DB_NAME);
+		template = new MongoTemplate(mongo, DB_NAME);
+		// DBCollection col = db.createCollection("projects", new
+		// BasicDBObject());
+
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		template.dropCollection(Project.class);
+	}
+
+	@Before
+	public void setUp() {
+
+		// 7
 		template.createCollection(Project.class);
 		RestAssured.port = port;
 		fNf = new Project();
@@ -121,29 +111,29 @@ public class ProjectRestControllerTest {
 		projectRepository.insert(compass);
 		// projectRepository.deleteAll();
 
-    }
-    @Test
-    public void testFindAllProjectsAndCheckSize(){
-    	
-        int projectsInCollection = template.findAll(Project.class).size();
-        
-        assertEquals("Only 2 projects should exist in collection, but there are " 
-                + projectsInCollection, 2, projectsInCollection);
+	}
 
-    }
-    
-   @Test
-    public void canFetchfNfProject() {
-        String projectId = fNf.getId();
+	@Test
+	public void testFindAllProjectsAndCheckSize() {
 
-        when().
-                get("/projects/{id}", projectId).
-        then().
-                statusCode(HttpStatus.SC_OK).
-                body("name", Matchers.is("F&F ecommerce")).
-                body("id", Matchers.is(projectId));
-    }
-   
+		int projectsInCollection = template.findAll(Project.class).size();
+
+		assertEquals(
+				"Only 3 projects should exist in collection, but there are "
+						+ projectsInCollection, 3, projectsInCollection);
+
+	}
+
+	@Test
+	public void canFetchfNfProject() {
+		String projectId = fNf.getId();
+
+		when().get("/projects/{id}", projectId).then()
+				.statusCode(HttpStatus.SC_OK)
+				.body("name", Matchers.is("F&F ecommerce"))
+				.body("id", Matchers.is(projectId));
+	}
+
 	@Test
 	public void canFetchCompassProject() {
 		String projectId = "cmpassId001";
@@ -153,16 +143,18 @@ public class ProjectRestControllerTest {
 				.body("name", Matchers.is("Compass Search Project"))
 				.body("id", Matchers.is(projectId));
 	}
-    
-   @Test
-    public void canFetchAll() {
-        when().
-                get("/projects").
-        then().
-                statusCode(HttpStatus.SC_OK).
-                body("name", Matchers.hasItems("F&F ecommerce", "HORIZON ecommerce platform"));
-    }
-   
+
+	@Test
+	public void canFetchAll() {
+		when().get("/projects")
+				.then()
+				.statusCode(HttpStatus.SC_OK)
+				.body("name",
+						Matchers.hasItems("F&F ecommerce",
+								"HORIZON ecommerce platform",
+								"Compass Search Project"));
+	}
+
 	@Test
 	public void canFetchProjectByName() {
 		String name = compass.getName();
@@ -175,17 +167,7 @@ public class ProjectRestControllerTest {
 								"spring boot"));
 
 	}
-    
-    @Test
-    public void canDeleteProjectfNf() {
-    	String projectId1 = fNf.getId();
 
-        when()
-                .delete("/projects/{id}", projectId1).
-        then().
-                statusCode(HttpStatus.SC_OK);
-    }
-    
 	@Test
 	public void canFetchByTagJavaSpringMongoDb() {
 		List<String> tags = Arrays.asList("java", "spring", "mongodb");
@@ -226,18 +208,32 @@ public class ProjectRestControllerTest {
 				.statusCode(HttpStatus.SC_OK).body("name", Matchers.empty());
 	}
 
+	@Test
+	public void canDeleteProjectHorizon() {
+		String projectId1 = horizon.getId();
 
-    
-    @Test
-    public void canDeleteProjectHorizon() {
-    	String projectId1 = horizon.getId();
+		when().delete("/projects/{id}", projectId1).then()
+				.statusCode(HttpStatus.SC_OK);
+	}
 
-        when()
-                .delete("/projects/{id}", projectId1).
-        then().
-                statusCode(HttpStatus.SC_OK);
-    }
-   
-    
+	@Test
+	public void testCreateNewProject() {
+		Project project = new Project();
+		project.setId("tempId");
+		project.setName("tempProject");
+		project.setTags(Arrays.asList("temp1", "temp2"));
+		RestAssured.given().contentType("application/json").body(project)
+				.when().post("/projects");
+
+		when().get("/projects/{id}", "tempId").then()
+				.statusCode(HttpStatus.SC_OK)
+				.body("name", Matchers.is("tempProject"))
+				.body("id", Matchers.is("tempId"))
+				.body("tags", Matchers.hasItems("temp1", "temp2"));
+
+		when().delete("/projects/{id}", "tempId").then()
+				.statusCode(HttpStatus.SC_OK);
+
+	}
+
 }
-
